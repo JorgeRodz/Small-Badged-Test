b9644966674951450de648bd64b18b2ef2dbbcdc
claunicole <claunicole.gf@gmail.com>
2022-08-16 21:55:59 -0400

    Refactor of the code and pass Rubycritic with 75
    co-authored-by: Cedako <mramos29@ucol.mx>
    co-authored-by: JorgeRodz <jarf4321@gmail.com>
    co-authored-by: Dcastanedaan <davinsonbmx@gmail.com>

--- game.rb
+++ game.rb
@@ -1,92 +1,84 @@
 # frozen_string_literal: true
+
 # Clase celula
 class Cell
-    attr_reader :status
-    def initialize
-        @type = ['*','.']
-        @status = @type[rand(2)]
-    end
+  attr_reader :status
 
+  def initialize
+    @type = ['*', '.']
+    @status = @type[rand(2)]
+  end
 end
 
 # Clase tablero
 class Grid
-    attr_accessor :grid, :row, :col
+  attr_accessor :grid, :row, :col
 
-    def initialize (row, col)
-        @row = row
-        @col = col
-        @grid = Array.new(@row){Array.new(@col) {Cell.new}}
+  def initialize(row, col)
+    @row = row
+    @col = col
+    @grid = Array.new(@row) { Array.new(@col) { Cell.new } }
+  end
 
+  def print_grid
+    @row.times do |i|
+      @col.times do |j|
+        print @grid[i][j].status
+      end
+      puts
     end
-
-    def print_grid
-        @row.times do |i|
-            @col.times do |j|
-                print @grid[i][j].status + ' '
-            end
-            puts
-        end
-        @grid
-    end
-
+    @grid
+  end
 end
 
 class Game
-    attr_accessor 
-    def initialize(grid)
-        @row = grid.size
-        @col = grid[0].size
-        @grid = grid
-        @new_grid = grid
-    end
+  attr_accessor
+  def initialize(grid)
+    @row = grid.size
+    @col = grid[0].size
+    @grid = grid
+    @new_grid = grid
+  end
 
-    #Recorre toda el grid
-    def run_matrix
-        p 'nueva matrix'
-        counter = 0
-        (0..@row - 1).each do |i|
-            (0..@col - 1).each do |j|
-                # print @grid[i][j].status, ' '
-                counter = neighbors(i,j).to_i
-                # print counter
-                print rules(i, j, counter, @grid[i][j].status) + " "
-            end
-            puts
+  # Recorre toda el grid
+  def run_matrix
+    p 'nueva matrix'
+    counter = 0
+    (0..@row - 1).each do |i|
+      (0..@col - 1).each do |j|
+        counter = neighbors(i, j).to_i
+        print rules(i, j, counter, @grid[i][j].status)
+      end
+      puts
+    end
+  end
+
+  # Recorre los vecinos recibiendo una celda
+  def neighbors(posx, posy)
+    neighbors = 0
+
+    # Recorre los vecinos
+    (-1..1).each do |k|
+      (-1..1).each do |l|
+        if !(k.zero? && l.zero?) && (posx + k >= 0 && posy + l >= 0) && (posx + k < @row && posy + l < @col)
+          @grid[posx + k][posy + l].status == "*" ? neighbors += 1 : neighbors += 0
         end
+      end
     end
+    neighbors
+  end
 
-    #Recorre los vecinos recibiendo una celda
-    def neighbors(posx,posy)
-        neighbors = 0
+  def rules(_pos_x, _pos_y, neighbors, status)
+    result_cell = '.'
 
-        #Recorre los vecinos
-        (-1..1).each do |k|
-            (-1..1).each do |l|
-                if !(k == 0 && l == 0) && (posx + k >= 0 && posy + l >= 0) && (posx + k < @row && posy + l < @col) #posx + k >= 0 && posy + l >= 0
-                    # print @grid[posx + k][posy + l].status + ' '
-                    @grid[posx + k][posy + l].status == "*" ? neighbors += 1 : neighbors += 0
-                    # neighbors += 1
-                end
-            end
-            # puts ''
-        end
-        return neighbors
+    if status == '*' && (neighbors < 2 || neighbors > 3)
+      result_cell = '.'
+    elsif status == '*' && neighbors >= 2 || status == '.' && neighbors == 3
+      result_cell = '*'
     end
-
-    def rules(pos_x, pos_y, neighbors, status)
-        if status == "*" && (neighbors < 2 || neighbors > 3)
-            return "."
-        elsif status == "*" && neighbors >= 2
-            return "*"
-        elsif status == "." && neighbors == 3
-            return "*"
-        else
-            return "."
-        end
-    end
+    result_cell
+  end
 end
-
 
 print 'Ingrese el numero de filas: '
 row = gets.chomp.to_i
@@ -97,5 +89,5 @@ col = gets.chomp.to_i
 grid = Grid.new(row, col)
 grid = grid.print_grid
 
-game= Game.new(grid)
+game = Game.new(grid)
 game.run_matrix
--- game_spec.rb
+++ game_spec.rb
@@ -6,10 +6,9 @@ require_relative './game.rb'
 # Evaluar estado de una celula
 
 describe Grid do
-    describe 'print_grid' do
-        it 'return an array with rows and cols' do
-            expect(Grid.new(5,5)).not_to eql(nil)
-        end    
-    end    
+  describe 'print_grid' do
+    it 'return an array with rows and cols' do
+      expect(Grid.new(5, 5)).not_to eql(nil)
+    end
+  end
 end
-
